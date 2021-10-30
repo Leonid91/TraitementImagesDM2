7a0174c812d3dc082e1cb93c1daa2f1e3e8caa4e
NS <aines.bouziane@yahoo.fr>
2021-10-30 15:33:58 +0200

    debug accumulateur

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt # A commenter si utilisation seulement de displayImg() définie ci-dessous.
+#import matplotlib.pyplot as plt # A commenter si utilisation seulement de displayImg() définie ci-dessous.
 import numpy as np
 import math
 import cv2
@@ -10,23 +10,18 @@ def displayImg(img):
 
 #OpenCV Version : opencv_python-4.5.4.58
 image = cv2.imread("images/fourn.png", cv2.IMREAD_GRAYSCALE)
-#displayImg(image)
 
 #1. Filtrage Gaussien (en cas de bruit ou de d ́etails très fins)
 blur_x = 5
 blur_y = 5
 blurredImg = cv2.GaussianBlur(image, (blur_x, blur_y), 0)
-#displayImg(blurredImg)
 
 #2. Filtrage de Sobel, calcul de la magnitude de gradient Imag dans chaque pixel
 sobelX = cv2.Sobel(blurredImg, cv2.CV_64F, 1, 0, ksize=5)  # X
-#displayImg(sobelX)
 sobelY = cv2.Sobel(blurredImg, cv2.CV_64F, 0, 1, ksize=5)  # Y
-#displayImg(sobelY)
 
 magnitude = np.sqrt((sobelX ** 2) + (sobelY ** 2))
 magnitude = magnitude / np.max(magnitude) # Pour ramener les valeurs des pixels dans l'intervalle [0, 1], pour que celà soit utilisable avec displayImg
-#displayImg(magnitude)
 
 # Une autre façon d'afficher les images : (à paraméter pour l'intervalle [0, 1]
 
@@ -36,7 +31,6 @@ magnitude = magnitude / np.max(magnitude) # Pour ramener les valeurs des pixels 
 #plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 #plt.subplot(2,2,1),plt.imshow(magnitude, cmap = 'gray')
 #plt.title('Magnitude'), plt.xticks([]), plt.yticks([])
-
 #plt.show()
 
 #3. Mise en contraste des contours
@@ -44,19 +38,19 @@ magnitude = magnitude / np.max(magnitude) # Pour ramener les valeurs des pixels 
 t = 0.26
 
 # Pour copier les valeurs > t (seuil) dans une matrice de copie
-matriceCopieSuperieur = np.ones_like(magnitude)
+matriceCopieSuperieur = np.zeros_like(magnitude)
 
 #s = 0.0
 for i in range(0, magnitude.shape[0]):
     for j in range(0, magnitude.shape[1]):
         if magnitude[i, j] > t:
-            magnitude[i, j] = 255
-            matriceCopieSuperieur[i, j] = magnitude[i, j]
-        else:
-            magnitude[i, j] = 0
+            #magnitude[i, j] = 255
+            matriceCopieSuperieur[i, j] = 1
+        #else:
+            #magnitude[i, j] = 0
         #s = s + magnitude[i, j]
 
-#displayImg(magnitude)
+#displayImg(matriceCopieSuperieur)
 
 # Une autre façon d'afficher :
 #plt.subplot(2,2,1),plt.imshow(magnitude, cmap = 'gray')
@@ -77,16 +71,20 @@ acc = np.zeros((I, J, int(K)))
 
 
 #5. Calcul du rayon rad pour que le cercle situé en (r, c) passe par le pixel respectif, incrementation dans l'accumulateur de la case qui correspond ) (r, c, rad)
-for row in range(0, magnitude.shape[0]):
-    for col in range(0, magnitude.shape[1]):
+for row in range(0, I):
+    for col in range(0, J):
         # Pas très sur si j'ai bien sélectionné les x et les y, il est possible que je les ai inversés, mais ça n'as peut-être pas d'importance pour le calcul
-        xi = magnitude[0][row]
-        yi = magnitude[col][0]
+        #xi = magnitude[0][row]
+        #yi = magnitude[col][0]
+        for xi in range (0, matriceCopieSuperieur.shape[0]):
+            for yi in range(0, matriceCopieSuperieur.shape[1]):
+                if matriceCopieSuperieur[xi][yi] > 0:
+                    rad = math.sqrt((xi - row)**2 + (yi - col)**2) ### On l'as vu dans le cours et je l'ai également trouvé sur internet cette formule
+                    acc[row][col][int(rad)] +=1
 
-        rad = math.sqrt((xi - row)**2 + (yi - col)**2) ### On l'as vu dans le cours et je l'ai également trouvé sur internet cette formule
-        acc[row][col][int(rad)] +=1
+        
 
-#print(acc)
+print(acc)
 
 #6 On cherche les maximums locaux
 locMax = ndimage.maximum_filter(acc, size=(1,1,1)) # dans un rayon d'un cube on a 26 cases voisines
@@ -114,12 +112,14 @@ N = 10
 selectedValues = np.argsort(locMaxNorm)[-N:, -N:, -N:] # Pour sélectionner N plus hautes valeurs triés par ordre décroissant
 #print(selectedValues) # Test...
 
+result = np.zeros_like(image)
+
 for x in range(selectedValues.shape[0]):
     for y in range(selectedValues.shape[1]):
         for z in range(selectedValues.shape[2]):
             a = int(selectedValues[0][x][0])
             b = int(selectedValues[y][0][0])
             rad = int(selectedValues[0][0][z])
-            cv2.circle(image, (a, b), rad, (0, 255, 0))
+            cv2.circle(result, (a, b), rad, (0, 255, 0))
 
-displayImg(image)
+displayImg(result)
